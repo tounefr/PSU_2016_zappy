705b163c489b4304d1696d0cb75ef2ea3c7ee88a
arsene <amaury.flament@epitech.eu>
2017-06-26 13:04:05 +0200

    added random ressources

--- src/server/resources.c
+++ src/server/resources.c
@@ -1,44 +1,52 @@
 /*
 ** resources.c for  in /home/toune/Documents/Epitech/projets/PSU_2016_zappy
-** 
+**
 ** Made by Thomas HENON
 ** Login   <thomas.henon@epitech.eu>
-** 
+**
 ** Started on  Sat Jun 24 16:09:57 2017 Thomas HENON
-** Last update Sat Jun 24 16:09:57 2017 Thomas HENON
+** Last update Mon Jun 26 12:02:49 2017 arsene
 */
 
 #include <stdio.h>
+#include <time.h>
 #include "server.h"
 
-char generate_resources(t_server *server)
+void	rand_ressource(t_server *server, int p)
+{
+  int	item;
+
+  item = (rand() % (b - a) + a);
+  if (item <= 56)
+    break;
+  else if (item <= 57 && item > 56)
+    server->map.cases[p][TYPE_THYSTAME]++;
+  else if (item <= 61 && item > 57)
+    server->map.cases[p][TYPE_MENDIANE]++;
+  else if (item <= 66 && item > 61)
+    server->map.cases[p][TYPE_PHIRAS]++;
+  else if (item <= 73 && item > 66)
+    server->map.cases[p][TYPE_SIBUR]++;
+  else if (item <= 80 && item > 73)
+    server->map.cases[p][TYPE_DERAUMERE]++;
+  else if (item <= 90 && item > 80)
+    server->map.cases[p][TYPE_LINEMATE]++;
+  else if (item <= 100 && item > 90)
+    server->map.cases[p][TYPE_FOOD]++;
+}
+
+char	generate_resources(t_server *server)
 {
     int x;
     int y;
     int p;
 
-
+    srand(time(NULL));
     for (y = 0; y < server->map.height; y++) {
         for (x = 0; x < server->map.width; x++) {
             p = y * server->map.height + x;
-            server->map.cases[p][TYPE_LINEMATE]++;
-            server->map.cases[p][TYPE_DERAUMERE]++;
-            server->map.cases[p][TYPE_SIBUR]++;
-            server->map.cases[p][TYPE_MENDIANE]++;
-            server->map.cases[p][TYPE_PHIRAS]++;
-            server->map.cases[p][TYPE_THYSTAME]++;
-            server->map.cases[p][TYPE_FOOD]++;
-
-            // temporaire
-            server->map.cases[p][TYPE_LINEMATE] = 100;
-            server->map.cases[p][TYPE_DERAUMERE] = 100;
-            server->map.cases[p][TYPE_SIBUR] = 100;
-            server->map.cases[p][TYPE_MENDIANE] = 100;
-            server->map.cases[p][TYPE_PHIRAS] = 100;
-            server->map.cases[p][TYPE_THYSTAME] = 100;
-            server->map.cases[p][TYPE_FOOD] = 100;
+	    rand_ressource(server, p);
         }
     }
-    printf("New ressources generated\n");
     return 1;
 }
