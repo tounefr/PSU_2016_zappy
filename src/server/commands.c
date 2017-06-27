6dba38892f3c5e378d8117aa792b3d7ecb9bbea2
arsene <amaury.flament@epitech.eu>
2017-06-27 11:27:10 +0200

    added file for globals

--- src/server/commands.c
+++ src/server/commands.c
@@ -1,54 +1,14 @@
 /*
 ** commands.c for  in /home/toune/Documents/Epitech/projets/PSU_2016_zappy/src/server
-** 
+**
 ** Made by Thomas HENON
 ** Login   <thomas.henon@epitech.eu>
-** 
+**
 ** Started on  Fri Jun 23 15:00:43 2017 Thomas HENON
-** Last update Fri Jun 23 15:00:44 2017 Thomas HENON
+** Last update Tue Jun 27 11:26:56 2017 arsene
 */
 
 #include "server.h"
-
-t_network_commands g_network_commands[N_NETWORK_COMMANDS] =
-{
-    { "Forward", NULL, onForwardPacket, 7, FLAG_NONE },
-    { "Right", NULL, onRightPacket, 7, FLAG_NONE },
-    { "Left", NULL, onLeftPacket, 7, FLAG_NONE },
-    { "Look", NULL, onLookPacket, 7, FLAG_NONE },
-    { "Inventory", NULL, onInventoryPacket, 1, FLAG_NONE },
-    { "Broadcast", NULL, onBroadcastPacket, 7, FLAG_NONE },
-    { "Connect_nbr", NULL, onConnectNbrPacket, 0, FLAG_NONE },
-    { "Fork", NULL, onForkPacket, 42, FLAG_NONE },
-    { "Eject", NULL, onEjectPacket, 7, FLAG_NONE },
-    { "Take", NULL, onTakeObjectPacket, 7, FLAG_NONE },
-    { "Set", NULL, onSetObjectPacket, 7, FLAG_NONE },
-    { "Incantation", onPreIncantationPacket, onPostIncantationPacket, 300, FLAG_NONE }
-};
-
-t_incantation g_incantations[NBR_LEVELS] =
-{
-    { 1, { 0, 1, 0, 0, 0, 0, 0 } },
-    { 2, { 0, 1, 1, 1, 0, 0, 0 } },
-    { 2, { 0, 2, 0, 1, 0, 2, 0 } },
-    { 4, { 0, 1, 1, 2, 0, 1, 0 } },
-    { 4, { 0, 1, 2, 1, 3, 0, 0 } },
-    { 6, { 0, 1, 2, 3, 0, 1, 0 } },
-    { 6, { 0, 2, 2, 2, 2, 2, 1 } }
-};
-
-t_food g_foods[RESOURCES_NBR_TYPES] =
-{
-    { "food", TYPE_FOOD },
-    { "thystame", TYPE_THYSTAME },
-    { "phiras", TYPE_PHIRAS },
-    { "mendiane", TYPE_MENDIANE },
-    { "sibur", TYPE_SIBUR },
-    { "deraumere", TYPE_DERAUMERE },
-    { "linemate", TYPE_LINEMATE },
-    { "food", TYPE_PLAYER },
-    { "egg", TYPE_EGG }
-};
 
 char    onSetObjectPacket(t_server *server, t_client *client, char *packet)
 {
---
+++ src/server/globals.c
@@ -0,0 +1,63 @@
+/*
+** globals.c for globals in /home/arsene/Desktop/{EPITECH}/PSU/PSU_2016_zappy/src/server
+**
+** Made by arsene
+** Login   <arsene@arsene-HP-EliteBook-840-G2>
+**
+** Started on  Tue Jun 27 11:19:54 2017 arsene
+** Last update Tue Jun 27 11:26:45 2017 arsene
+*/
+
+#include "server.h"
+
+t_food				*get_g_foods()
+{
+  static t_food			g_foods[RESOURCES_NBR_TYPES] =
+    {
+      { "food", TYPE_FOOD },
+      { "thystame", TYPE_THYSTAME },
+      { "phiras", TYPE_PHIRAS },
+      { "mendiane", TYPE_MENDIANE },
+      { "sibur", TYPE_SIBUR },
+      { "deraumere", TYPE_DERAUMERE },
+      { "linemate", TYPE_LINEMATE },
+      { "food", TYPE_PLAYER },
+      { "egg", TYPE_EGG }
+    }
+  return (g_foods);
+}
+
+t_incantation			*get_g_incantations()
+{
+  static t_incantation		g_incantations[NBR_LEVELS] =
+    {
+      { 1, { 0, 1, 0, 0, 0, 0, 0 } },
+      { 2, { 0, 1, 1, 1, 0, 0, 0 } },
+      { 2, { 0, 2, 0, 1, 0, 2, 0 } },
+      { 4, { 0, 1, 1, 2, 0, 1, 0 } },
+      { 4, { 0, 1, 2, 1, 3, 0, 0 } },
+      { 6, { 0, 1, 2, 3, 0, 1, 0 } },
+      { 6, { 0, 2, 2, 2, 2, 2, 1 } }
+    }
+  return (g_incantations);
+}
+
+t_network_commands		*g_network_commands()
+{
+  static t_network_commands	g_network_commands[N_NETWORK_COMMANDS] =
+    {
+      { "Forward", NULL, onForwardPacket, 7, FLAG_NONE },
+      { "Right", NULL, onRightPacket, 7, FLAG_NONE },
+      { "Left", NULL, onLeftPacket, 7, FLAG_NONE },
+      { "Look", NULL, onLookPacket, 7, FLAG_NONE },
+      { "Inventory", NULL, onInventoryPacket, 1, FLAG_NONE },
+      { "Broadcast", NULL, onBroadcastPacket, 7, FLAG_NONE },
+      { "Connect_nbr", NULL, onConnectNbrPacket, 0, FLAG_NONE },
+      { "Fork", NULL, onForkPacket, 42, FLAG_NONE },
+      { "Eject", NULL, onEjectPacket, 7, FLAG_NONE },
+      { "Take", NULL, onTakeObjectPacket, 7, FLAG_NONE },
+      { "Set", NULL, onSetObjectPacket, 7, FLAG_NONE },
+      { "Incantation", onPreIncantPacket, onPostIncantPacket, 300, FLAG_NONE }
+    }
+  return (g_network_commands);
+}
