9d1429624f0eedd254a350f7176ab4c20feb3dfc
arsene <amaury.flament@epitech.eu>
2017-06-29 17:15:47 +0200

    look

--- src/server/look.c
+++ src/server/look.c
@@ -5,30 +5,71 @@
 ** Login   <arsene@arsene-HP-EliteBook-840-G2>
 **
 ** Started on  Mon Jun 26 14:18:29 2017 arsene
-** Last update Wed Jun 28 16:57:02 2017 arsene
+** Last update Thu Jun 29 17:02:28 2017 arsene
 */
 
+#include <stdlib.h>
+#include <string.h>
 #include "server.h"
 
-char		*get_tiles_infos(int pos, t_server *server)
+char		*get_type(int i)
+{
+  if (i == TYPE_FOOD)
+    return ("food");
+  else if (i == TYPE_LINEMATE)
+    return ("linemate");
+  else if (i == TYPE_DERAUMERE)
+    return ("deraumere");
+  else if (i == TYPE_SIBUR)
+    return ("sibur");
+  else if (i == TYPE_MENDIANE)
+    return ("mendiane");
+  else if (i == TYPE_PHIRAS)
+    return ("phiras");
+  else if (i == TYPE_THYSTAME)
+    return ("thystame");
+  else if (i == TYPE_PLAYER)
+    return ("player");
+  else if (i == TYPE_EGG)
+    return ("egg");
+}
+
+char		*get_tiles_infos(int pos, t_server *server, char *message)
 {
   int		i;
+  int		count;
 
-  /* for (i = 0; i < RESOURCES_NBR_TYPES; i++) */
-  /*   printf("%s\n", get_g_foods()[i].s); */
-  /* printf("%s\n", server->map.cases[pos][get_g_foods()[i].s]); */
-  /* char		*buffer; */
-  /* int		i; */
-  /* int		count_buffer; */
+  count = 0;
+  for (i = 0; i < RESOURCES_NBR_TYPES; i++)
+    {
+      if (server->map.cases[pos][i] > 0)
+	{
+	  while (server->map.cases[pos][i] > count)
+	    {
+	      if (count > 0)
+		{
+		  message = realloc(message, strlen(message) + 2);
+		  message = strcat(message, " ");
+		}
+	      printf("WHAT IS ABOUT TO BE WRITTEN : %s\n", get_type(i));
+	      sleep(1);
 
-  /* count_buffer = 0; */
-  /* for (i = 0; i < RESOURCES_NBR_TYPES; i++) */
-  /*   { */
-  /*     if (server->map.cases[pos][get_g_foods()[i].type] > 0) */
-  /* 	{ */
-  /* 	  while (count_buffer < ) */
-  /* 	} */
-  /*   } */
+	      if (message != NULL)
+		message = realloc(message, strlen(message) + strlen(get_type(i)) + 1);
+	      else
+		message = realloc(message, strlen(get_type(i)) + 1);
+	      message = strcat(message, get_type(i));
+	      count++;
+	    }
+	}
+    }
+  printf("pos : %d\n", pos);
+  if (message != NULL)
+    message = realloc(message, strlen(message) + 2);
+  else
+    message = realloc(message, 2);
+  message = strcat(message, ",");
+  return (message);
 }
 
 char		*look(t_client *client, t_server *server)
@@ -41,11 +82,9 @@ char		*look(t_client *client, t_server *server)
   int		front;
 
   i = 0;
-  j = 0;
-  vision = 0;
-  printf("/!\\ LOOK FUNCTION\n /!\\\n");
-  printf("/!\\ LOOK FUNCTION\n /!\\\n");
-  printf("/!\\ LOOK FUNCTION\n /!\\\n");
+  j = 1;
+  field_of_view = malloc(1);
+  field_of_view = '\0';
   curr_pos = client->pos.x + (client->pos.y * server->map.height);
   if (client->orientation == ORIENT_NORTH ||
       client->orientation == ORIENT_SOUTH)
@@ -53,25 +92,26 @@ char		*look(t_client *client, t_server *server)
       while (i <= client->level)
 	{
 	  if (client->orientation == ORIENT_NORTH)
-	    {
-	      front = curr_pos - (i * server->map.width);
-	      printf("FRONT NORTH IS : %d\n", front);
-	    }
+	    front = curr_pos - (i * server->map.width);
 	  if (client->orientation == ORIENT_SOUTH)
+	    front = curr_pos + (i * server->map.width);
+
+
+	  field_of_view = get_tiles_infos(front, server, field_of_view);
+	  printf("FRONT\n");
+
+	  if (i != 0)
 	    {
-	      sleep(2);
-	      printf("CLIENT LEVEL : %d\n", client->level);
-	      front = curr_pos + (i * server->map.width) + 1;
-	      printf("FRONT SOUTH IS : %d\n", front);
+	      while (j <= i)
+		{
+		  field_of_view = get_tiles_infos(front - j, server, field_of_view);
+		  printf("LEFT\n");
+		  field_of_view = get_tiles_infos(front + j, server, field_of_view);
+		  printf("RIGHT\n");
+		  j++;
+		}
 	    }
-	  get_tiles_infos(front, server);
-	  while (j <= client->level)
-	    {
-	      get_tiles_infos(front + j, server);
-	      get_tiles_infos(front - j, server);
-	      j++;
-	    }
-	  j = 0;
+	  j = 1;
 	  i++;
 	}
     }
@@ -81,24 +121,27 @@ char		*look(t_client *client, t_server *server)
       while (i <= client->level)
 	{
 	  if (client->orientation == ORIENT_EAST)
-	    {
-	      front = curr_pos + i;
-	      printf("FRONT EAST IS : %d\n", front);
-	    }
+	    front = curr_pos + i;
 	  if (client->orientation == ORIENT_WEST)
-	    {
-	      front = curr_pos - i;
-	      printf("FRONT WEST IS : %d\n", front);
-	    }
-	  get_tiles_infos(front, server);
+	    front = curr_pos - i;
+
+	  field_of_view = get_tiles_infos(front, server, field_of_view);
+
 	  while (j <= client->level)
 	    {
-	      get_tiles_infos(front + (j * server->map.width), server);
-	      get_tiles_infos(front - (j * server->map.width), server);
+	      field_of_view = get_tiles_infos(front + (j * server->map.width)
+					      , server, field_of_view);
+	      field_of_view = get_tiles_infos(front - (j * server->map.width)
+					      , server, field_of_view);
 	      j++;
 	    }
 	  j = 0;
 	  i++;
 	}
     }
-}
+  if (field_of_view != NULL)
+    {
+      field_of_view[strlen(field_of_view) - 1] = '\0';
+      packet_send(client, "%s", field_of_view);
+    }
+    }
