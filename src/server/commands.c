364ebb79eda8931a1856d874e53208e7ea83148a
Thomas HENON <thomas.henon@epitech.eu>
2017-07-02 17:58:46 +0200

    fix shit

--- src/server/commands.c
+++ src/server/commands.c
@@ -10,19 +10,18 @@
 
 #include "server.h"
 
-char    onSetObjectPacket(t_server *server, t_client *client, char *packet)
-{
+char onSetObjectPacket(t_server *server, t_client *client, char *packet) {
     int i;
     char food[100];
     int client_pos;
     t_callback *callback;
 
-    memset((char*)&food, 0, sizeof(food));
-    strncpy((char*)&food, packet + 4, sizeof(food));
+    memset((char *) &food, 0, sizeof(food));
+    strncpy((char *) &food, packet + 4, sizeof(food));
     client_pos = client->pos.x + client->pos.y * server->map.width;
     for (i = 0; i < RESOURCES_NBR_TYPES; i++) {
-        if ((!is_stone(i) && strcmp((char*)&food, "food")) ||
-                strcmp(get_g_foods()[i].s, (char*)&food))
+        if ((!is_stone(i) && strcmp((char *) &food, "food")) ||
+            strcmp(get_g_foods()[i].s, (char *) &food))
             continue;
         if (client->inventory[get_g_foods()[i].type] - 1 < 0)
             return packet_send(client, "ko\n");
@@ -40,45 +39,41 @@ char    onSetObjectPacket(t_server *server, t_client *client, char *packet)
 }
 
 
-char    onTakeObjectPacket(t_server *server, t_client *cli, char *packet)
-{
-  int i;
-  char food[100];
-  int client_pos;
-  t_callback *cb;
+char onTakeObjectPacket(t_server *server, t_client *cli, char *packet) {
+    int i;
+    char food[100];
+    int client_pos;
+    t_callback *cb;
 
-  memset(&food, 0, sizeof(food));
-  strncpy((char*)&food, packet + 5, sizeof(food) - 1);
-  client_pos = cli->pos.x + cli->pos.y * server->map.width;
-  for (i = 0; i < RESOURCES_NBR_TYPES; i++)
-    {
-      if ((!is_stone(i) && strcmp((char*)&food, "food")) ||
-	  strcmp(get_g_foods()[i].s, (char*)&food))
-	continue;
-      if (server->map.cases[client_pos][get_g_foods()[i].type] - 1 < 0)
-	return packet_send(cli, "ko\n");
-      cli->inventory[get_g_foods()[i].type]++;
-      take_ressource(server, client_pos, i);
-      send_gui_packet(server, "pgt %d %d\n", cli->num, get_g_foods()[i].type);
-      if (get_g_foods()[i].type == TYPE_FOOD &&
-	  (cb = get_callback(cli, onPlayerDead)))
-	cb->cycles += CYCLES_PER_LIFE_UNIT;
-      gui_send_map_case(server, cli->pos.x, cli->pos.y);
-      return packet_send(cli, "ok\n");
+    memset(&food, 0, sizeof(food));
+    strncpy((char *) &food, packet + 5, sizeof(food) - 1);
+    client_pos = cli->pos.x + cli->pos.y * server->map.width;
+    for (i = 0; i < RESOURCES_NBR_TYPES; i++) {
+        if ((!is_stone(i) && strcmp((char *) &food, "food")) ||
+            strcmp(get_g_foods()[i].s, (char *) &food))
+            continue;
+        if (server->map.cases[client_pos][get_g_foods()[i].type] - 1 < 0)
+            return packet_send(cli, "ko\n");
+        cli->inventory[get_g_foods()[i].type]++;
+        take_ressource(server, client_pos, i);
+        send_gui_packet(server, "pgt %d %d\n", cli->num, get_g_foods()[i].type);
+        if (get_g_foods()[i].type == TYPE_FOOD &&
+            (cb = get_callback(cli, onPlayerDead)))
+            cb->cycles += CYCLES_PER_LIFE_UNIT;
+        gui_send_map_case(server, cli->pos.x, cli->pos.y);
+        return packet_send(cli, "ok\n");
     }
-  return packet_send(cli, "ko\n");
+    return packet_send(cli, "ko\n");
 }
 
-char    onPreForkPacket(t_server *server, t_client *client, char *packet)
-{
-    (void)packet;
+char onPreForkPacket(t_server *server, t_client *client, char *packet) {
+    (void) packet;
     send_gui_packet(server, "pfk %d\n", client->num);
     return 1;
 }
 
-char    onPostForkPacket(t_server *server, t_client *client, char *packet)
-{
-    (void)packet;
+char onPostForkPacket(t_server *server, t_client *client, char *packet) {
+    (void) packet;
     if (!lay_egg(server, client))
         return packet_send(client, "ko\n");
     return packet_send(client, "ok\n");
